b220e4b4eaf9258dafef96b7a1b8c24e8a575c31
galiciatechnologies <galiciatechnologies@gmail.com>
2019-06-30 14:54:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-essx76-u8toki
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-essx76-u8toki)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    image: '1',
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    image: '2',
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    image: '3',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
